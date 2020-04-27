c67f2ac9a8e79d9cd345078a041713bfccd37508
Moseskhan <34588306+Moseskhan@users.noreply.github.com>
2020-04-27 19:15:00 -0400

    added  filters for variouis domains.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const port = 3000;
-
+const psl = require('psl');
 var bodyParser = require('body-parser')
 var cors = require('cors');
 var app = express();
@@ -18,13 +18,14 @@ app.post('/images', jsonParser, function(req, res) {
         .then(res => res.text())
         .then(async body => {
             let dateTime = new Date();
-            
+
             console.log('make http req', dateTime.getMilliseconds());
             var $ = cheerio.load(body);
             //title of the page
             var title = $("title").text();
             title = title.toLocaleLowerCase();
-
+            title = title.toString().replace('\t', '');
+            title = title.toString().replace('\n', '');
             if (title === 'robot check') {
                 await fetch(url)
                     .then(res => res.text())
@@ -38,14 +39,45 @@ app.post('/images', jsonParser, function(req, res) {
 
             //images present in the html page
             var imageUrls = [];
+            dateTime = new Date();
             console.log('parse results', dateTime.getMilliseconds());
+
             $('img').each(function(i, element) {
                 var a = $(this);
 
-                console.log(title);
                 var image = a.attr('src');
+                if (image == undefined) {
+                    image = a.attr('data-src');
+                }
+                if (image.startsWith('/') || !image.startsWith('h')) {
+
+                    let url = req.body.url;
+                    var extractDomain = (url) => {
+                        var domain;
+                        //find & remove protocol (http, ftp, etc.) and get domain
+                        if (url.indexOf("://") > -1) {
+                            domain = url.split('/')[2];
+                        } else {
+                            domain = url.split('/')[0];
+                        }
+
+                        //find & remove www
+                        if (domain.indexOf("www.") > -1) {
+                            domain = domain.split('www.')[1];
+                        }
+
+                        domain = domain.split(':')[0]; //find & remove port number
+                        domain = domain.split('?')[0]; //find & remove url params
+
+                        return domain;
+                    }
+                    image = "https://" + extractDomain(url) + "/" + image;
+
+                }
+
+                console.log(image);
                 //avoid data image urls because of the size---avaerage size of image url is 150
-                if (image != undefined && image.length < 150) {
+                if (image != undefined && image.length < 150 && !image.startsWith('data:image') && !image.includes('logo')) {
                     let filtergGif = image.endsWith('gif');
                     let filterSvg = image.endsWith('svg');
                     // console.log(filtergGif, image);
@@ -53,9 +85,10 @@ app.post('/images', jsonParser, function(req, res) {
                     //filter svg and gif which are mostly icons or loaders
                     if (!filterSvg && !filtergGif) {
                         // add unique images only
-                        if(imageUrls.indexOf(image) === -1
-                        && image.length > 0){
-                            if(image[0] === '/' && image[1] === '/'){
+                        if (imageUrls.indexOf(image) === -1 &&
+                            image.length > 0
+                        ) {
+                            if (image[0] === '/' && image[1] === '/') {
                                 image = `https:${image}`;
                             }
                             imageUrls.push(image);
@@ -63,6 +96,7 @@ app.post('/images', jsonParser, function(req, res) {
                     }
                 }
             });
+
 
             //additional filters for specific domains
             //amazon filter
@@ -75,11 +109,83 @@ app.post('/images', jsonParser, function(req, res) {
                         imageUrls.push(x);
                     }
                 });
+
                 console.log(images);
                 if (imageUrls.length == 0) {
                     imageUrls = images;
                 }
             }
+
+            //gap filter
+            if (url.includes('gap.com')) {
+                let images = imageUrls;
+                imageUrls = [];
+                images.map(x => {
+                    if (x.includes('webcontent')) {
+                        imageUrls.push("https://www.gap.com/" + x)
+                    }
+
+                })
+                if (imageUrls.length == 0) {
+                    imageUrls = images.map(x => {
+                        return "https://www.gap.com/" + x;
+                    });
+                }
+            }
+
+            //kohls filter
+            if (url.includes('kohls.com')) {
+                let images = imageUrls;
+                imageUrls = [];
+                images.map(x => {
+                    if (!x.startsWith('/')) {
+                        imageUrls.push(x);
+                    }
+                })
+            }
+
+            //arizona tiles
+            if (url.includes('arizonatile.com')) {
+                let images = imageUrls;
+                imageUrls = [];
+                images.map(x => {
+                    if (x.startsWith('/') && !x.includes('Assets') && !x.includes('media/icons')) {
+                        imageUrls.push('https://www.arizonatile.com/' + x);
+                    }
+                })
+            }
+
+            //lumber liquitdators
+
+            if (url.includes('lumberliquidators.com')) {
+                let images = imageUrls;
+                imageUrls = [];
+                images.map(x => {
+                    if (!x.startsWith('/')) {
+                        imageUrls.push(x);
+                    }
+                })
+            }
+
+            //tile bar filter
+            if (url.includes('tilebar.com')) {
+                let images = imageUrls;
+                imageUrls = [];
+                images.map(x => {
+                    if (x.includes('product')) {
+                        imageUrls.push(x);
+                    }
+                });
+                if (imageUrls.length == 0) {
+                    imageUrls = images.map(x => {
+                        return x;
+                    });
+                }
+            }
+
+
+
+
 
             //return object
             let returnObj = {
